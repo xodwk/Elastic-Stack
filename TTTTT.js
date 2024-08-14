ba05e81644d24953ebec25fd66283e0cb302bc15
xodwk <123256506+xodwk@users.noreply.github.com>
2024-08-15 00:59:24 +0900

    Delete TTTTT.js

--- TTTTT.js
+++
@@ -1,9 +0,0 @@
-function vulnerableFunction(userInput) {
-    // 취약점: XSS
-    document.getElementById("output").innerHTML = userInput;
-}
-
-function secureFunction(userInput) {
-    // 보안: HTML 엔코딩
-    document.getElementById("output").textContent = userInput;
-}
